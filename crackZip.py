eaa95bed4ce60f51ba3f44bb9598553c730f467b
root <root@localhost>
2020-08-16 19:37:52 +0000

    main

---
+++ crackZip.py
@@ -0,0 +1,37 @@
+import os
+import zipfile
+import sys
+from time import sleep
+os.system('clear')
+print('''
+
+ ______     ______     ______     ______     __  __     ______     __     ______  
+/\  ___\   /\  == \   /\  __ \   /\  ___\   /\ \/ /    /\___  \   /\ \   /\  == \ 
+\ \ \____  \ \  __<   \ \  __ \  \ \ \____  \ \  _"-.  \/_/  /__  \ \ \  \ \  _-/ 
+ \ \_____\  \ \_\ \_\  \ \_\ \_\  \ \_____\  \ \_\ \_\   /\_____\  \ \_\  \ \_\   
+  \/_____/   \/_/ /_/   \/_/\/_/   \/_____/   \/_/\/_/   \/_____/   \/_/   \/_/   
+                                                                                  
+
+
+Author    ===>> king
+Instagram ===>> hacking_with_king
+GitHub    ===>> KING3140
+\n
+
+''')
+if len(sys.argv)!=3:
+	print(''' Usage option > python3 crackZip.py <path of zipfile> <path of dictionary>''')
+	sys.exit()
+def crack():
+	zip_file=sys.argv[1]
+	wordlist=sys.argv[2]
+	wordlist=open(wordlist,'r',encoding="ISO-8859-1")
+	zip_file=zipfile.ZipFile(zip_file)
+	for word in wordlist:
+		try:
+			zip_file.extractall(pwd=word.strip())
+			print('[+] Password found ===>>> '+word,'\n[+] File extracted ')
+			sys.exit()
+		except:
+			pass
+crack()
